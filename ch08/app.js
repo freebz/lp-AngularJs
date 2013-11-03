acb8663672e2de910ee90a7d57910192d5ef0827
FreeBz <freebz@hananet.net>
2013-11-04 07:11:38 +0900

    Cheatsheet and Recipes

---
+++ ch08/app.js
@@ -0,0 +1,14 @@
+$(function(){
+    $("#datepicker").datepicker();
+    $("#datePicker").datepicker();
+});
+
+var app = angular.module('myApp', ['myApp.directives']);
+
+app.controller('MainCtrl', function($scope) {
+    $scope.myText = 'Not Selected';
+    $scope.currentDate = '';
+    $scope.updateMyText = function(date) {
+	$scope.myText = 'Selected';
+    };
+});
