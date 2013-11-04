5eec98f88837d1dc247306208bb0cb4348ec3d02
FreeBz <freebz@hananet.net>
2013-11-04 23:24:34 +0900

    File Upload in AngularJS

---
+++ ch08/ex03.js
@@ -0,0 +1,44 @@
+angular.module('myApp.directives', [])
+    .directive('fileupload', function() {
+	return {
+	    restrict: 'A',
+	    scope: {
+		done: '&',
+		progress: '&'
+	    },
+	    link: function(scope, element, attrs) {
+		var optionsObj = {
+		    dataType: 'json'
+		};
+
+		if (scope.done) {
+		    optionsObj.done = function() {
+			scope.$apply(function() {
+			    scope.done({e: e, data: data});
+			});
+		    };
+		}
+
+		if (scope.progress) {
+		    optionsObj.progress = function(e, data) {
+			scope.$apply(function() {
+			    scope.progress({e: e, data: data});
+			});
+		    }
+		}
+
+		// the above colud easily be done in a loop, to cover
+		// all API's that Fileupload provides
+
+		element.fileupload(optionsObj);
+	    }
+	};
+    });
+
+var app = angular.module('myApp', ['myApp.directives']);
+
+app.controller('MainCtrl', function($scope) {
+    $scope.uploadFinished = function(e, data) {
+	console.log('We just finished uploading this baby...');
+    };
+});
